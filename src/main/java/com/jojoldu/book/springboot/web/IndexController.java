2ee72f3ae9a43e08650658de2a9491ce2ef31271
Cat-hanbit <sweat0713@gmail.com>
2021-03-08 01:14:13 +0900

    테스트

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -23,7 +23,7 @@ public class IndexController {
     public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
         if (user != null) {
-            model.addAttribute("user", user.getName());
+            model.addAttribute("userInfo", user.getName());
         }
         return "index";
     }
